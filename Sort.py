e5fa805249241572841ec871d3c8812bc7cb7302
pshenglh <psheng@outlook.com>
2017-07-28 13:34:01 +0800

    Merge Sort

--- Sort.py
+++ Sort.py
@@ -37,6 +37,73 @@ class Shell:
                     exch(list, j, j-h)
             h = h / 3
 
+
+class Merge:
+
+    def merge(self, a, lo, mid, hi):
+        i = lo
+        j = mid + 1
+        for k in range(lo, hi+1):
+            self.aux[k] = a[k]
+
+        for k in range(lo, hi+1):
+            if i > mid:
+                a[k] = self.aux[j]
+                j += 1
+            elif j > hi:
+                a[k] = self.aux[i]
+                i += 1
+            elif self.aux[j] < self.aux[i]:
+                a[k] = self.aux[j]
+                j += 1
+            else:
+                a[k] = self.aux[i]
+                i += 1
+
+    def recurse_sort(self, a, lo, hi):
+        if hi <= lo: return
+        mid = lo + (hi - lo) / 2
+        self.recurse_sort(a, lo, mid)
+        self.recurse_sort(a, mid+1, hi)
+        self.merge(a, lo, mid, hi)
+
+    def sort(self, a):
+        self.aux = list(range(len(a)))
+        self.recurse_sort(a, 0, len(a)-1)
+
+
+class MergeBU:
+
+    def merge(self, a, lo, mid, hi):
+        i = lo
+        j = mid + 1
+
+        for k in range(lo, hi+1):
+            self.aux[k] = a[k]
+
+        for k in range(lo, hi+1):
+            if i > mid:
+                a[k] = self.aux[j]
+                j += 1
+            elif j > hi:
+                a[k] = self.aux[i]
+                i += 1
+            elif self.aux[j] < self.aux[i]:
+                a[k] = self.aux[j]
+                j += 1
+            else:
+                a[k] = self.aux[i]
+                i += 1
+
+    def sort(self, a):
+        n = len(a)
+        self.aux = list(range(len(a)))
+        sz = 1
+        while sz < n:
+            for lo in range(0, n-sz, 2*sz):
+                self.merge(a, lo, lo+sz-1, min(lo+2*sz, n-1))
+            sz *= 2
+
 class Test:
 
     def test_seletion(self, list):
@@ -51,13 +118,21 @@ class Test:
         shell = Shell()
         shell.sort(list)
 
+    def test_merge(self, list):
+        merge = Merge()
+        merge.sort(list)
+
+    def test_mergebu(self, list):
+        merge = MergeBU()
+        merge.sort(list)
+
 def exch(list, i, j):
     tmp = list[j]
     list[j] = list[i]
     list[i] = tmp
 
 if __name__ == '__main__':
-    list = In('tiny.txt').read_strings()
+    a = In('tiny.txt').read_strings()
     test = Test()
-    test.test_shell(list)
-    print list
+    test.test_mergebu(a)
+    print a
